4d1fa6d7d456a451446a8a94cdbb45532818232f
dangnh-dev <haidang14727@gmail.com>
2022-05-10 23:09:32 +0700

    .

--- src/main/java/com/example/todoapi/config/WebSecurityConfig.java
+++ src/main/java/com/example/todoapi/config/WebSecurityConfig.java
@@ -42,8 +42,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors();
-        http.csrf().disable()
+        http.cors().and().csrf().disable()
             .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
             .authorizeRequests().antMatchers("/api/auth/**").permitAll()
--- src/main/java/com/example/todoapi/controllers/HomeController.java
+++ src/main/java/com/example/todoapi/controllers/HomeController.java
@@ -26,6 +26,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+@CrossOrigin(origins = "http://128.199.158.139:8080", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
 public class HomeController {
