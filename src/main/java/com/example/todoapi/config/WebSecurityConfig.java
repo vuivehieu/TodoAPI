89cf13f53dfbb61ddf52eec50202ff535a2c90cb
dangnh-dev <haidang14727@gmail.com>
2022-05-10 22:29:04 +0700

    .

--- src/main/java/com/example/todoapi/config/WebSecurityConfig.java
+++ src/main/java/com/example/todoapi/config/WebSecurityConfig.java
@@ -42,7 +42,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http.cors().and().csrf().disable();
+        http.cors().and().csrf().disable();
         http.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
             .authorizeRequests().antMatchers("/api/auth/**").permitAll()
