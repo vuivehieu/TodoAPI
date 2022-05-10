91fc4f470203d0397dedabf0bf54119fe8700347
vuivehieu <75554300+vuivehieu@users.noreply.github.com>
2022-05-10 16:22:51 +0700

    fix

--- src/main/java/com/example/todoapi/repositories/NoteRepository.java
+++ src/main/java/com/example/todoapi/repositories/NoteRepository.java
@@ -2,11 +2,13 @@ package com.example.todoapi.repositories;
 
 import com.example.todoapi.entities.NoteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface NoteRepository extends JpaRepository<NoteEntity, Long> {
+    @Query("SELECT a From NoteEntity a where a.note_description like %:noteDes%")
     List<NoteEntity> findAllByNote_description(String noteDes);
 }
