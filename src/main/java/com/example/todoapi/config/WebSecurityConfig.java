357fe686f3dcfc71487d4b9ec34538e8f0e466bc
dangnh-dev <haidang14727@gmail.com>
2022-05-11 00:46:53 +0700

    .

--- src/main/java/com/example/todoapi/config/WebSecurityConfig.java
+++ src/main/java/com/example/todoapi/config/WebSecurityConfig.java
@@ -51,15 +51,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         final CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.unmodifiableList(Arrays.asList("localhost:3000")));
-        configuration.setAllowedMethods(Collections.unmodifiableList(Arrays.asList("HEAD",
-                "GET", "POST", "PUT", "DELETE", "PATCH")));
+        configuration.setAllowedOrigins(Arrays.asList("localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD",
+                "GET", "POST", "PUT", "DELETE", "PATCH"));
         // setAllowCredentials(true) is important, otherwise:
         // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
         configuration.setAllowCredentials(true);
         // setAllowedHeaders is important! Without it, OPTIONS preflight request
         // will fail with 403 Invalid CORS request
-        configuration.setAllowedHeaders(Collections.unmodifiableList(Arrays.asList("Authorization", "Cache-Control", "Content-Type")));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
